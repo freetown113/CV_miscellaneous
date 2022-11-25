5364ce6477c37ef94486f42bd86ff34e70437a42
freetown113 <freetown113@gmail.com>
2022-11-25 15:16:31 +0200

    remove get_patch_size(), add save_image(), refactored according to PEP8

--- split_merge/split.py
+++ split_merge/split.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 import numpy as np
+import os
 from PIL import Image
-from typing import Tuple
 
 ### ERROR handling and undefined input handling
 ### EXCEPTIONS right handling behaviour
@@ -12,57 +12,98 @@ class Slicer:
                  height: int,
                  width: int,
                  x_shift: int,
-                 y_shift: int
+                 y_shift: int,
+                 output_path: str
                  ) -> None:
-        self.height = height
-        self.width = width
-        self.cols = x_shift
-        self.rows = y_shift
         self.image = self.__open_image(image_path)
+        self.height = self.__check_values(height, 'height')
+        self.width = self.__check_values(width, 'width')
+        self.cols = self.__check_values(x_shift, 'x_shift')
+        self.rows = self.__check_values(y_shift, 'y_shift')
+        self.out_path = self.__check_dir(output_path)
+
+    def __check_dir(self, path):
+        if not os.path.exists(path):
+            os.makedirs(path)
+        return path
 
     def __open_image(self,
                      image_path: str
                      ) -> np.array:
         return np.array(Image.open(image_path))
 
-    def get_patch_sizes(self) -> Tuple[int, int, int, int]:
-        h, w, c = self.image.shape
-        return (*divmod(h, self.height), *divmod(w, self.width))
+    def __check_values(self, arg, name):
+        if not isinstance(arg, int):
+            raise ValueError(f'Provide {name} argument as an int, you \
+                               provided argument of type {type(arg)}')
+        h, w = self.image.shape[:2]
+        if name == 'height' and arg > h or name == 'width' and arg > w:
+            raise ValueError(f'Patch size cannot be more than image size \
+                               by any dimension. You provided patch height: \
+                               {self.height} and width: {self.width}, \
+                               while image height: {h} and width: {w}')
+        if (name == 'height' or name == 'width') and arg <= 0:
+            raise ValueError(f'Patch size cannot be less than 1 by any \
+                               dimension. You provided {arg} value for \
+                               {name} argument.')
+        if (name == 'x_shift' or name == 'y_shift') and arg < 0:
+            raise ValueError(f'The shift cannot be negative by any \
+                               dimension. You provided {arg} value for \
+                               {name} argument.')
+        else:
+            return arg
 
     def slice(self):
-        rows, row_spare, cols, col_spare = self.get_patch_sizes()
+        h, w = self.image.shape[:2]
+        row_spare, col_spare = h % self.height, w % self.width
 
         v_chunks = self.split_reminder(self.image, self.height, axis=0) \
             if row_spare else self.split_evenly(self.image, self.height,
                                                 axis=0)
 
-        for v_idx, chank in enumerate(v_chunks):
+        for w_idx, chank in enumerate(v_chunks):
             h_chunks = self.split_reminder(chank, self.width, axis=1) \
                 if col_spare else self.split_evenly(self.image, self.width,
                                                     axis=1)
             for h_idx, img in enumerate(h_chunks):
-                Image.fromarray(img).save('output/' + str(v_idx) + '_' +
-                                          str(h_idx) + '_image.png')
+                self.save_image(img, w_idx, h_idx)
 
-    def split_evenly(self, array, chunk_size, axis=0):
+    def split_evenly(self,
+                     array: np.array,
+                     chunk_size: int,
+                     axis: int = 0
+                     ) -> np.array:
         return np.array_split(array, np.ceil(array.shape[axis] / chunk_size),
                               axis=axis)
 
-    def split_reminder(self, array, chunk_size, axis=0):
+    def split_reminder(self,
+                       array: np.array,
+                       chunk_size: int,
+                       axis: int = 0
+                       ) -> np.array:
         indices = np.arange(chunk_size, array.shape[axis], chunk_size)
         return np.array_split(array, indices, axis)
+
+    def save_image(self,
+                   array: np.array,
+                   row: int,
+                   col: int
+                   ) -> None:
+        Image.fromarray(array).save(os.path.join(self.out_path, str(row)
+                                                 + '_' + str(col) +
+                                                 '_image.jpg'))
 
 
 def main(args):
     slicer = Slicer(args.image, args.height, args.width, args.x_shift,
-                    args.y_shift)
+                    args.y_shift, args.output)
     slicer.slice()
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--image', help='path to image',
-                        default='image.png')
+                        default='Cape_Town.jpg', type=str)
     parser.add_argument('--height', help='height of a patch',
                         default=300, type=int)
     parser.add_argument('--width', help='width of a patch',
@@ -71,5 +112,7 @@ if __name__ == '__main__':
                         default=0, type=int)
     parser.add_argument('--y_shift', help='shift by y coodinate',
                         default=0, type=int)
+    parser.add_argument('--output', help='result images directory',
+                        default='split_output', type=str)
     arguments = parser.parse_args()
     main(arguments)
